dcb8c8ddf94c25efd1c907b991d4a3e38238ec95
Tit1e <evollyone@outlook.com>
2021-09-29 16:00:15 +0800

    program init

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-# Kindle2Flomo
+# send2flomo
 
-> 本软件已获得 lupeng 授权感谢 [lupeng](https://github.com/pengloo53/kindle2flomo) 的授权
+## Project setup
+```
+yarn install
+```
 
-将 Kindle 笔记一键导入至 flomo
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
 
-## 注册 [flomo](https://flomoapp.com/register2/?MTAzNDE)
+### Compiles and minifies for production
+```
+yarn build
+```
 
-## 在线访问地址：[Kindle2Flomo](https://tit1e.github.io/kindle2Flomo/)
-
-## Mac 应用下载
-* [蓝奏云](https://wwr.lanzoui.com/b02c3nkyf) 密码:47if
-* [releases](https://github.com/Tit1e/kindle2Flomo/releases)
-
-## 问题反馈
-![](https://personal-1251959693.cos.ap-chengdu.myqcloud.com/2021-08-09-kindle2flomo%E9%97%AE%E9%A2%98%E5%8F%8D%E9%A6%88.png)
-
-## 预览
-![](./screenshot/mac.png)
-
-![](./screenshot/web.png)
-
-
-## 开发注意事项
-**bplistParser** 这个依赖需手动修改 `maxObjectSize` 与 `maxObjectCount` 这两个常量的数值，修改得大一些，否则当 `Books.plist` 中图书数量过多时会出现无法解析的问题。
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/background.ts
+++ src/background.ts
@@ -2,14 +2,14 @@
 
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
-app.allowRendererProcessReuse = true
+
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
@@ -18,8 +18,7 @@ async function createWindow() {
     frame: false,
     titleBarStyle: 'hidden',
     webPreferences: {
-      // 禁用安全策略
-      webSecurity: false,
+
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
@@ -60,7 +59,7 @@ app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS_DEVTOOLS)
+      await installExtension(VUEJS3_DEVTOOLS)
     } catch (e) {
       console.error('Vue Devtools failed to install:', e.toString())
     }
